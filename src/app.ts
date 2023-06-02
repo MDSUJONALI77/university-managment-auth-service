dc90d7ec0994e8245bec7845c04ec831cab609ea
Md. Sujon Ali <sujonrashid944@gmail.com>
2023-06-02 01:32:13 -0400

    [DONE] user-interface model service route utils controller

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
+import router from './app/module/user/user.route'
 const app: Application = express()
 
 app.use(cors())
@@ -7,7 +8,9 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req: Request, res: Response) => {
+app.use('/api/v1/users', router)
+
+app.get('/', async (req: Request, res: Response) => {
   res.send('Hello World!')
 })
 
