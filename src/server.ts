89bd07c1f14b2430edfe66e11501cb72488cb95b
Md. Sujon Ali <sujonrashid944@gmail.com>
2023-06-03 01:31:27 -0400

    [DONE] setup logger

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,17 @@
 import app from './app'
 import mongoose from 'mongoose'
 import config from './config'
+import { logger, ErrorLogger } from './shared/logger'
 
 async function main() {
   try {
     await mongoose.connect(config.database_url as string)
-    console.log('database is connect successfully')
+    logger.info('database is connect successfully')
     app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`)
+      logger.info(`Example app listening on port ${config.port}`)
     })
   } catch (err) {
-    console.log('database is not found ', err)
+    ErrorLogger.error('database is not found ', err)
   }
 }
 
